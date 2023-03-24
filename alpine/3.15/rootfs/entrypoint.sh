dc4e50676d94a76da470f7194b83a37b96915d45
Robert <robert@diehoerrs.de>
2023-03-24 14:41:07 +0100

    add root

--- alpine/3.15/rootfs/entrypoint.sh
+++ alpine/3.15/rootfs/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$ENV_VS_EXTENSIONS_GALLERY" = true ]; then
-    export EXTENSIONS_GALLERY='{"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery"}'
-fi
-
 if [ "$ENV_WEB_PASSWORD" != "" ]; then
     SALT=`cat /dev/urandom | head -c 10 | base64`
     export HASHED_PASSWORD=`echo -n "$ENV_WEB_PASSWORD" | argon2 $SALT -e`
@@ -13,19 +9,42 @@ if [ "$ENV_WEB_PASSWORD" != "" ]; then
     unset SALT
 fi
 
-if [ "$ENV_USER_PASSWORD" != "" ]; then
-    echo -e "${ENV_USER_PASSWORD}\n${ENV_USER_PASSWORD}" | passwd $ENV_USER_NAME  > /dev/null 2> /dev/null
-    unset ENV_USER_PASSWORD
+##set run user password
+if  [ "$ENV_USER_PASSWORD" != "" ]; then
+    echo -e "${ENV_USER_PASSWORD}\n${ENV_USER_PASSWORD}" | passwd $ENV_RUN_USER  > /dev/null 2> /dev/null
+fi
 
-    echo "$ENV_USER_NAME ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers.d/nopasswd > /dev/null
+##set run user sudo
+if [ "$ENV_USER_PASSWORD" != "" ] && [ "$ENV_RUN_USER" != "root" ]; then
+    echo "$ENV_RUN_USER ALL=(ALL:ALL) ALL" | tee -a /etc/sudoers.d/nopasswd > /dev/null
+    unset ENV_USER_PASSWORD
 fi
 
 if [ "$ENV_GIT_NAME" != "" ] && [ "$ENV_GIT_EMAIL" != "" ]; then
-    /sbin/su-exec $ENV_USER_NAME git config --global user.name $ENV_GIT_NAME
-    /sbin/su-exec $ENV_USER_NAME git config --global user.email $ENV_GIT_EMAIL
+    /sbin/su-exec $ENV_RUN_USER git config --global user.name $ENV_GIT_NAME
+    /sbin/su-exec $ENV_RUN_USER git config --global user.email $ENV_GIT_EMAIL
     unset ENV_GIT_NAME
     unset ENV_GIT_EMAIL
 fi
+
+if [ "$ENV_VS_EXTENSIONS_GALLERY" = true ]; then
+    export EXTENSIONS_GALLERY='{"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery"}'
+fi
+
+
+
+#TODO: caddy ssl proxy
+#mydomain.com
+#reverse_proxy 0.0.0.0:8443
+
+unset ENV_UID
+unset ENV_GID
+unset ENV_VS_EXTENSIONS_GALLERY
+
+
+exec /sbin/su-exec $ENV_RUN_USER "$@"
+
+
 
 #if [ ! -f "$/app/config/certs" ]; then
     #cd /app/config/certs
@@ -41,14 +60,3 @@ fi
 #    rm -r /app/config/.oh-my-zsh
 #    sed -i -e "s/bin\/zsh/bin\/ash/" /etc/passwd
 #fi
-
-#TODO: caddy ssl proxy
-#mydomain.com
-#reverse_proxy 0.0.0.0:8443
-
-unset ENV_OH_MY_ZSH
-unset ENV_UID
-unset ENV_VS_EXTENSIONS_GALLERY
-
-###service
-exec /sbin/su-exec $ENV_USER_NAME "$@"
--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 #--no-cache 
 #docker image -f prune
 #docker system prune -a -f
+mkdir rootfs/app/workspace
 #docker build -t test .
-docker run --rm -it -e ENV_USER_PASSWORD=hallo -e ENV_WEB_PASSWORD=hallo -e ENV_GIT_NAME=robert -e ENV_GIT_EMAIL=rij@jj.de test bash
+docker run --rm -it -p 8443:8443 \
+-e ENV_RUN_USER=root -e ENV_WORKSPACE=/mnt -e ENV_USER_PASSWORD=hallo -e ENV_WEB_PASSWORD=hallo \
+-e ENV_GIT_NAME=robert -e ENV_GIT_EMAIL=rij@jj.de test
